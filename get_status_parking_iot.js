fc60b9086db953da08c3723c1df122ccd084c681
n u n e <noonunenoonune@gmail.com>
2021-03-07 01:10:51 +0700

    Create get_status_parking_iot.js

---
+++ get_status_parking_iot.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+
+app.use(express.json())
+
+app.post('/get_status_parking_lot', (req, res) => {
+    const { floor } = req.body
+
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'parking_iot'
+    })
+
+    con.connect((err) => {
+        if(err){
+            console.error(err.stack)
+            return
+        }
+
+        var query_get_status = "SELECT * FROM db_parking WHERE floor = '"+floor+"'"
+        con.query(query_get_status, (ree,result) => {
+            if(err){
+                console.error(err.stack)
+                return
+            }
+
+            res.json({
+                result: result[0]
+            })
+        })
+    })
+})
+
+app.listen(3000)
